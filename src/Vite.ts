a72d214b83f1dd800d409e6370ef79f47e9d9ca6
Stone <stone.hsia@gmail.com>
2020-01-20 17:46:46 +0800

    tidy

--- src/Vite.ts
+++ src/Vite.ts
@@ -2,20 +2,13 @@ import Transport from "@ledgerhq/hw-transport";
 import BIPPath from "bip32-path";
 import { accountBlock as AccountBlock } from '@vite/vitejs';
 
-export declare type int = number;
 export declare type Hex = string;
 export declare type Address = string;
 export declare type Base64 = string;
 export declare type TokenId = string;
-export declare type Int32 = string;
-export declare type Int64 = string;
-export declare type Uint8 = string;
 export declare type Uint16 = string;
-export declare type Uint32 = string;
 export declare type Uint64 = string;
-export declare type Uint256 = string;
 export declare type BigInt = string;
-export declare type Bytes32 = string;
 
 export default class Vite {
     transport: Transport;
@@ -24,17 +17,194 @@ export default class Vite {
         this.transport = transport;
     }
 
+    async getAppConfig(): Promise<{
+        version: string,
+        builtinTokenCount: Uint16
+    }> {
+
+        const cla = 0xa1;
+        const ins = 0x01;
+        const p1 = 0x00;
+        const p2 = 0x00;
+
+        let buf = Buffer.alloc(0);
+        buf = await this.transport.send(cla, ins, p1, p2, buf);
+
+        let version = `${buf[0]}.${buf[1]}.${buf[2]}`;
+        let builtinTokenCount = `${buf[3] * 256 + buf[4]}`;
+
+        return {
+            version,
+            builtinTokenCount
+        };
+    }
+
+    async getBuiltinTokenInfo(
+        index: number
+    ): Promise<{
+        tokenId: TokenId,
+        decimals: number,
+        symbolAndIndex: string
+    }> {
+        const cla = 0xa1;
+        const ins = 0x06;
+        const p1 = 0x00;
+        const p2 = 0x00;
+
+        let buf = Buffer.from([index / 256, index % 256]);
+        buf = await this.transport.send(cla, ins, p1, p2, buf);
+
+        const tokenId = buf.slice(0, 28).toString("ascii");
+        const decimals = buf[28];
+        const symbolAndIndex = buf.slice(29, buf.length - 2).toString("ascii");
+
+        return {
+            tokenId,
+            decimals,
+            symbolAndIndex
+        };
+    }
+
+    async getTestAmountText(
+        amount: BigInt, 
+        tokenId: TokenId
+    ): Promise<string> {
+
+        const cla = 0xa1;
+        const ins = 0x07;
+        const p1 = 0x00;
+        const p2 = 0x00;
+
+        let size = 32; // amount
+        size += 10; // tokenId
+
+        let ptr = 0;
+        let buf = Buffer.alloc(size);
+
+        ptr += buf.write(AccountBlock.utils.getAmountHex(amount), ptr, buf.length - ptr, "hex");
+        ptr += buf.write(AccountBlock.utils.getTokenIdHex(tokenId), ptr, buf.length - ptr, "hex");
+        buf = await this.transport.send(cla, ins, p1, p2, buf);
+        const text = buf.slice(0, buf.length - 2).toString("ascii");
+        return text;
+    }
+
+    async getAddress(
+        accountIndex: number,
+        boolDisplay: boolean = false
+    ): Promise<{
+        publicKey: Base64,
+        address: Address
+    }> {
+        const path = `44'/666666'/${accountIndex}'`;
+        const bipPath = BIPPath.fromString(path).toPathArray();
+
+        const cla = 0xa1;
+        const ins = 0x02;
+        const p1 = boolDisplay ? 0x01 : 0x00;
+        const p2 = 0x00;
+
+        let size = 1 + 4 * bipPath.length; // bipPath
+
+        let buf = Buffer.alloc(size);
+        buf.writeUInt8(bipPath.length, 0);
+        bipPath.forEach((segment, index) => {
+            buf.writeUInt32BE(segment, 1 + 4 * index);
+        });
+
+        buf = await this.transport.send(cla, ins, p1, p2, buf);
+        let ptr = 0;
+
+        ptr += 32;
+        const publicKey = buf.slice(ptr - 32, ptr).toString("base64");
+
+        const addressLength = buf.readUInt8(ptr);
+        ptr += 1 + addressLength;
+        const address = buf.slice(ptr - addressLength, ptr).toString("ascii");
+
+        return {
+            publicKey,
+            address
+        };
+    }
+
+    async signReceiveAccountBlock(
+        accountIndex: number,
+        height: Uint64,
+        fromHash: Hex,
+        prevHash?: Hex,
+        nonce?: Base64
+    ): Promise<{
+        blockHash: Hex,
+        signature: Base64
+    }> {
+        const path = `44'/666666'/${accountIndex}'`;
+        const bipPath = BIPPath.fromString(path).toPathArray();
+
+        const cla = 0xa1;
+        const ins = 0x03;
+        const p1 = 0x00;
+        const p2 = 0x00;
+
+        let size = 1 + 4 * bipPath.length; // bipPath
+        size += 32; // prevHash
+        size += 8; // height
+        size += 32; // fromHash
+        size += 8; // nonce
+
+        let ptr = 0;
+        let buf = Buffer.alloc(size);
+
+        buf.writeUInt8(bipPath.length, ptr);
+        ptr += 1;
+        bipPath.forEach((segment, _) => {
+            buf.writeUInt32BE(segment, ptr);
+            ptr += 4;
+        });
+
+        if (prevHash) {
+            ptr += buf.write(prevHash, ptr, buf.length - ptr, "hex");
+        } else {
+            ptr += buf.write("0000000000000000000000000000000000000000000000000000000000000000", ptr, buf.length - ptr, "hex");
+        }
+
+        ptr += buf.write(AccountBlock.utils.getHeightHex(height), ptr, buf.length - ptr, "hex");
+        ptr += buf.write(fromHash, ptr, buf.length - ptr, "hex");
+
+        if (nonce) {
+            ptr += buf.write(nonce, ptr, buf.length - ptr, "base64");
+        } else {
+            ptr += buf.write("0000000000000000", ptr, buf.length - ptr, "hex");
+        }
+
+        buf = await this.transport.send(cla, ins, p1, p2, buf);
+        ptr = 0;
+
+        ptr += 32;
+        const blockHash = buf.slice(ptr - 32, ptr).toString("hex");
+
+        ptr += 64;
+        const signature = buf.slice(ptr - 64, ptr).toString("base64");
+
+        return {
+            blockHash,
+            signature
+        };
+    }
+
     async signSendAccountBlock(
-        accountIndex: int,
+        accountIndex: number,
         height: Uint64,
         toAddress: Address,
         amount: BigInt,
         tokenId: TokenId,
-        prevHash?: Hex,
-        fee?: BigInt,
         data?: Base64,
-        nonce?: Base64
-    ) {
+        fee?: BigInt,
+        prevHash?: Hex,
+        nonce?: Base64,
+    ): Promise<{
+        blockHash: Hex,
+        signature: Base64
+    }> {
 
         const dataBuffer = data ? Buffer.from(data, 'base64') : Buffer.alloc(0);
         let dataBufferPtr = 0;
@@ -136,172 +306,5 @@ export default class Vite {
             }
         }
         return await send.call(this)
-    }
-
-    async signReceiveAccountBlock(
-        accountIndex: int,
-        height: Uint64,
-        fromHash: Hex,
-        prevHash?: Hex,
-        nonce?: Base64
-    ): Promise<{
-        blockHash: Hex,
-        signature: Base64
-    }> {
-        const path = `44'/666666'/${accountIndex}'`;
-        const bipPath = BIPPath.fromString(path).toPathArray();
-
-        const cla = 0xa1;
-        const ins = 0x03;
-        const p1 = 0x00;
-        const p2 = 0x00;
-
-        let size = 1 + 4 * bipPath.length; // bipPath
-        size += 32; // prevHash
-        size += 8; // height
-        size += 32; // fromHash
-        size += 8; // nonce
-
-        let ptr = 0;
-        let buf = Buffer.alloc(size);
-
-        buf.writeUInt8(bipPath.length, ptr);
-        ptr += 1;
-        bipPath.forEach((segment, _) => {
-            buf.writeUInt32BE(segment, ptr);
-            ptr += 4;
-        });
-
-        if (prevHash) {
-            ptr += buf.write(prevHash, ptr, buf.length - ptr, "hex");
-        } else {
-            ptr += buf.write("0000000000000000000000000000000000000000000000000000000000000000", ptr, buf.length - ptr, "hex");
-        }
-
-        ptr += buf.write(AccountBlock.utils.getHeightHex(height), ptr, buf.length - ptr, "hex");
-        ptr += buf.write(fromHash, ptr, buf.length - ptr, "hex");
-
-        if (nonce) {
-            ptr += buf.write(nonce, ptr, buf.length - ptr, "base64");
-        } else {
-            ptr += buf.write("0000000000000000", ptr, buf.length - ptr, "hex");
-        }
-
-        buf = await this.transport.send(cla, ins, p1, p2, buf);
-        ptr = 0;
-
-        ptr += 32;
-        const blockHash = buf.slice(ptr - 32, ptr).toString("hex");
-
-        ptr += 64;
-        const signature = buf.slice(ptr - 64, ptr).toString("base64");
-
-        return {
-            blockHash,
-            signature
-        };
-    }
-
-
-    async getAddress(
-        accountIndex: int,
-        boolDisplay?: Boolean
-    ): Promise<{
-        publicKey: Base64,
-        address: Address
-    }> {
-        const path = `44'/666666'/${accountIndex}'`;
-        const bipPath = BIPPath.fromString(path).toPathArray();
-
-        const cla = 0xa1;
-        const ins = 0x02;
-        const p1 = boolDisplay ? 0x01 : 0x00;
-        const p2 = 0x00;
-
-        let size = 1 + 4 * bipPath.length; // bipPath
-
-        let buf = Buffer.alloc(size);
-        buf.writeUInt8(bipPath.length, 0);
-        bipPath.forEach((segment, index) => {
-            buf.writeUInt32BE(segment, 1 + 4 * index);
-        });
-
-        buf = await this.transport.send(cla, ins, p1, p2, buf);
-        let ptr = 0;
-
-        ptr += 32;
-        const publicKey = buf.slice(ptr - 32, ptr).toString("base64");
-
-        const addressLength = buf.readUInt8(ptr);
-        ptr += 1 + addressLength;
-        const address = buf.slice(ptr - addressLength, ptr).toString("ascii");
-
-        return {
-            publicKey,
-            address
-        };
-    }
-
-
-    async getAppConfig(): Promise<{
-        version: string,
-        builtinTokenCount: Uint16
-    }> {
-
-        const cla = 0xa1;
-        const ins = 0x01;
-        const p1 = 0x00;
-        const p2 = 0x00;
-
-        let buf = Buffer.alloc(0);
-        buf = await this.transport.send(cla, ins, p1, p2, buf);
-
-        let version = `${buf[0]}.${buf[1]}.${buf[2]}`;
-        let builtinTokenCount = `${buf[3] * 256 + buf[4]}`;
-
-        return {
-            version,
-            builtinTokenCount
-        };
-    }
-
-    async getBuiltinTokenInfo(index) {
-        const cla = 0xa1;
-        const ins = 0x06;
-        const p1 = 0x00;
-        const p2 = 0x00;
-
-        let buf = Buffer.from([index / 256, index % 256]);
-        buf = await this.transport.send(cla, ins, p1, p2, buf);
-
-        const tokenId = buf.slice(0, 28).toString("ascii");
-        const decimals = buf[28];
-        const symbolAndIndex = buf.slice(29, buf.length - 2).toString("ascii");
-
-        return {
-            tokenId,
-            decimals,
-            symbolAndIndex
-        };
-    }
-
-    async getTestAmountText(amount, tokenId) {
-
-        const cla = 0xa1;
-        const ins = 0x07;
-        const p1 = 0x00;
-        const p2 = 0x00;
-
-        let size = 32; // amount
-        size += 10; // tokenId
-
-        let ptr = 0;
-        let buf = Buffer.alloc(size);
-
-        ptr += buf.write(AccountBlock.utils.getAmountHex(amount), ptr, buf.length - ptr, "hex");
-        ptr += buf.write(AccountBlock.utils.getTokenIdHex(tokenId), ptr, buf.length - ptr, "hex");
-        buf = await this.transport.send(cla, ins, p1, p2, buf);
-        const text = buf.slice(0, buf.length - 2).toString("ascii");
-        return text;
     }
 }
--- tests/Vite.test.js
+++ tests/Vite.test.js
@@ -100,7 +100,7 @@ async function signSendBlockAndSend(accountAddress, publicKey, toAddress, amount
     difficulty = null;
   }
 
-  const signResult = await vite.signSendAccountBlock(accountIndex, previousHash, height, toAddress, amount, tokenId, fee, data, nonce)
+  const signResult = await vite.signSendAccountBlock(accountIndex, height, toAddress, amount, tokenId, data, fee, previousHash, nonce)
   const blockHash = signResult.blockHash;
   const signature = signResult.signature;
 
@@ -177,7 +177,7 @@ async function signReceiveBlockAndSend(accountAddress, publicKey, sendBlockHash)
       difficulty = null;
     }
 
-    const signResult = await vite.signReceiveAccountBlock(accountIndex, previousHash, height, sendBlockHash, nonce);
+    const signResult = await vite.signReceiveAccountBlock(accountIndex, height, sendBlockHash, previousHash, nonce);
     const blockHash = signResult.blockHash;
     const signature = signResult.signature;
 
@@ -226,45 +226,45 @@ async function getOnlineAllBuiltinTokenInfo() {
   return tokenInfos;
 }
 
-// test("vite.generateOnlineAllBuiltinTokenInfo", async () => {
+test("vite.generateOnlineAllBuiltinTokenInfo", async () => {
 
-//   const tokenInfos = await getOnlineAllBuiltinTokenInfo();
-//   console.log("count" ,tokenInfos.length)
+  const tokenInfos = await getOnlineAllBuiltinTokenInfo();
+  console.log("count" ,tokenInfos.length)
 
-//   let text = "";
-//   for (let index = 0; index < tokenInfos.length; index++) {
-//     const token = tokenInfos[index];
+  let text = "";
+  for (let index = 0; index < tokenInfos.length; index++) {
+    const token = tokenInfos[index];
 
-//     let buffer = Buffer.from(AccountBlock.utils.getTokenIdHex(token.tokenId), "hex");
-//     let raw = `{`;
+    let buffer = Buffer.from(AccountBlock.utils.getTokenIdHex(token.tokenId), "hex");
+    let raw = `{`;
     
-//     raw += `0x${buffer.slice(0,1).toString("hex")},`;
-//     raw += `0x${buffer.slice(1,2).toString("hex")},`;
-//     raw += `0x${buffer.slice(2,3).toString("hex")},`;
-//     raw += `0x${buffer.slice(3,4).toString("hex")},`;
-//     raw += `0x${buffer.slice(4,5).toString("hex")},`;
-//     raw += `0x${buffer.slice(5,6).toString("hex")},`;
-//     raw += `0x${buffer.slice(6,7).toString("hex")},`;
-//     raw += `0x${buffer.slice(7,8).toString("hex")},`;
-//     raw += `0x${buffer.slice(8,9).toString("hex")},`;
-//     raw += `0x${buffer.slice(9,10).toString("hex")}}`;
+    raw += `0x${buffer.slice(0,1).toString("hex")},`;
+    raw += `0x${buffer.slice(1,2).toString("hex")},`;
+    raw += `0x${buffer.slice(2,3).toString("hex")},`;
+    raw += `0x${buffer.slice(3,4).toString("hex")},`;
+    raw += `0x${buffer.slice(4,5).toString("hex")},`;
+    raw += `0x${buffer.slice(5,6).toString("hex")},`;
+    raw += `0x${buffer.slice(6,7).toString("hex")},`;
+    raw += `0x${buffer.slice(7,8).toString("hex")},`;
+    raw += `0x${buffer.slice(8,9).toString("hex")},`;
+    raw += `0x${buffer.slice(9,10).toString("hex")}}`;
 
-//     text += "{";
-//     text += raw;
-//     text += ", ";
-//     text += '"';
-//     text += token.tokenSymbol
-//     if (token.tokenSymbol != 'VITE' && token.tokenSymbol != 'VCP' && token.tokenSymbol != 'VX') {
-//       text += "-";
-//       text += `${token.index}`.padStart(3, '0');
-//     }
-//     text += '"';
-//     text += ", ";
-//     text += `${token.decimals}`;
-//     text += "},\n";
-//   }
-//   console.log(text);
-// }, 50000000);
+    text += "{";
+    text += raw;
+    text += ", ";
+    text += '"';
+    text += token.tokenSymbol
+    if (token.tokenSymbol != 'VITE' && token.tokenSymbol != 'VCP' && token.tokenSymbol != 'VX') {
+      text += "-";
+      text += `${token.index}`.padStart(3, '0');
+    }
+    text += '"';
+    text += ", ";
+    text += `${token.decimals}`;
+    text += "},\n";
+  }
+  console.log(text);
+}, 50000000);
 
 test("vite.getAppConfig", async () => {
   const result = await vite.getAppConfig();
@@ -277,135 +277,138 @@ test("vite.getAppConfig", async () => {
     text += `${result.tokenId}, ${result.symbolAndIndex}, ${result.decimals}\n`;
   }
   console.log(`tokenInfo\n${text}`);
+
+  const amountText = await vite.getTestAmountText('1234567890123456789', 'tti_5649544520544f4b454e6e40');
+  console.log(`amountText: ${amountText}`);
 }, 50000000);
 
-// test("vite.getTestAmountText", async () => {
+test("vite.getTestAmountText", async () => {
 
-//   function getAmount(count) {
-//     if (count == 0) {
-//       return '0';
-//     }
+  function getAmount(count) {
+    if (count == 0) {
+      return '0';
+    }
 
-//     const text = '1234567890';
-//     let amount = '';
-//     for (let index = 0; index < count; index++) {
-//       amount += text[index%10];
-//     }
+    const text = '1234567890';
+    let amount = '';
+    for (let index = 0; index < count; index++) {
+      amount += text[index%10];
+    }
 
-//     return amount;
-//   }
+    return amount;
+  }
 
-//   function amountToBaseString(amount, decimals) {
-//     const min = new BigNumber(10).exponentiatedBy(decimals);
-//     amount = new BigNumber(amount);
-//     if (amount.c === null) {
-//         return '';
-//     }
-//     try {
-//         return amount.dividedBy(min).toFormat();
-//     } catch (err) {
-//         return '';
-//     }
-//   }
+  function amountToBaseString(amount, decimals) {
+    const min = new BigNumber(10).exponentiatedBy(decimals);
+    amount = new BigNumber(amount);
+    if (amount.c === null) {
+        return '';
+    }
+    try {
+        return amount.dividedBy(min).toFormat();
+    } catch (err) {
+        return '';
+    }
+  }
 
-//   function uniqueSymbol(token) {
-//     if (token.tokenSymbol == 'VITE' || token.tokenSymbol == 'VCP' || token.tokenSymbol == 'VX') {
-//       return token.tokenSymbol;
-//     } else {
-//       return `${token.tokenSymbol}-`+`${token.index}`.padStart(3, '0');
-//     }
-//   }
+  function uniqueSymbol(token) {
+    if (token.tokenSymbol == 'VITE' || token.tokenSymbol == 'VCP' || token.tokenSymbol == 'VX') {
+      return token.tokenSymbol;
+    } else {
+      return `${token.tokenSymbol}-`+`${token.index}`.padStart(3, '0');
+    }
+  }
 
-//   const tokenInfos = await getOnlineAllBuiltinTokenInfo();
+  const tokenInfos = await getOnlineAllBuiltinTokenInfo();
   
-//   let maxDecimals = 0;
-//   for (let index = 0; index < tokenInfos.length; index++) {
-//     const tokenInfo = tokenInfos[index];
-//     if (tokenInfo.decimals > maxDecimals) {
-//       maxDecimals = tokenInfo.decimals;
-//     }
-//   }
+  let maxDecimals = 0;
+  for (let index = 0; index < tokenInfos.length; index++) {
+    const tokenInfo = tokenInfos[index];
+    if (tokenInfo.decimals > maxDecimals) {
+      maxDecimals = tokenInfo.decimals;
+    }
+  }
 
-//   BigNumber.config({ DECIMAL_PLACES: maxDecimals, FORMAT: {groupSeparator: '', decimalSeparator: '.'} })
+  BigNumber.config({ DECIMAL_PLACES: maxDecimals, FORMAT: {groupSeparator: '', decimalSeparator: '.'} })
 
-//   console.log("maxDecimals" , maxDecimals);
+  console.log("maxDecimals" , maxDecimals);
 
-//   for (let index = 0; index < tokenInfos.length; index++) {
-//     const tokenInfo = tokenInfos[index];
-//     console.log(`test ${uniqueSymbol(tokenInfo)} ${tokenInfo.decimals}`);
-//     for (let index = 0; index < maxDecimals * 2; index++) {
-//       const amount = getAmount(index);
+  for (let index = 0; index < tokenInfos.length; index++) {
+    const tokenInfo = tokenInfos[index];
+    console.log(`test ${uniqueSymbol(tokenInfo)} ${tokenInfo.decimals}`);
+    for (let index = 0; index < maxDecimals * 2; index++) {
+      const amount = getAmount(index);
 
-//       const result = uniqueSymbol(tokenInfo) + ' ' + amountToBaseString(amount, tokenInfo.decimals)
-//       const test = await vite.getTestAmountText(amount, tokenInfo.tokenId);
-//       // console.log('amount', amount, 'result', result, 'test', test);
-//       expect(result).toEqual(test);
-//     }
-//   }
-// }, 50000000);
+      const result = uniqueSymbol(tokenInfo) + ' ' + amountToBaseString(amount, tokenInfo.decimals)
+      const test = await vite.getTestAmountText(amount, tokenInfo.tokenId);
+      // console.log('amount', amount, 'result', result, 'test', test);
+      expect(result).toEqual(test);
+    }
+  }
+}, 50000000);
 
 let accountIndex = 0;
 
-// test("vite.getAddress", async () => {
-//   await getAddress(accountIndex, false);
-//   await getAddress(accountIndex, true);
-// }, 50000000);
+test("vite.getAddress", async () => {
+  await getAddress(accountIndex, false);
+  await getAddress(accountIndex, true);
+}, 50000000);
 
-// test("vite.signReceiveAccountBlock", async () => {
-//   const accountResult = await vite.getAddress(accountIndex, false);
-//   console.log(accountResult.publicKey);
-//   console.log(accountResult.address);
+test("vite.signReceiveAccountBlock", async () => {
+  const accountResult = await vite.getAddress(accountIndex, false);
+  console.log(accountResult.publicKey);
+  console.log(accountResult.address);
 
-//   const signResult = await vite.signReceiveAccountBlock(0, null, 1, "1e3004d74382a8635b836eb8a3e34ede7c00a7a1bff0c150974c1235287ad07a", "4KVvCafscbA=");
-//   console.log(signResult.blockHash);
-//   console.log(signResult.signature);
+  const signResult = await vite.signReceiveAccountBlock(0, 1, "1e3004d74382a8635b836eb8a3e34ede7c00a7a1bff0c150974c1235287ad07a", null, "4KVvCafscbA=");
+  console.log(signResult.blockHash);
+  console.log(signResult.signature);
 
-// }, 50000000);
+}, 50000000);
 
-// test("vite.signSendAccountBlock", async () => {
-//   try {
-//     const _accountResult = await getAddress(accountIndex, false);
-//     let publicKey = _accountResult.publicKey;
-//     let accountAddress = _accountResult.address;
+test("vite.signSendAccountBlock", async () => {
+  try {
+    const _accountResult = await getAddress(accountIndex, false);
+    let publicKey = _accountResult.publicKey;
+    let accountAddress = _accountResult.address;
 
-//     const toAddress = "vite_847e1672c9a775ca0f3c3a2d3bf389ca466e5501cbecdb7107";
-//     const tokenId = "tti_5649544520544f4b454e6e40";
-//     const fee = "0";
+    const toAddress = "vite_847e1672c9a775ca0f3c3a2d3bf389ca466e5501cbecdb7107";
+    const tokenId = "tti_5649544520544f4b454e6e40";
+    const fee = "0";
 
-//     const notes = [
-//       // null,
-//       // "",
-//       // "abcdefghijklmnopqrstuvwxyz",
-//       // "abcdefghijklmnopqrstuvwxyz_ABCDEFGHIJKLMNOPQRSTUVWXYZ-0123456789",
-//       // "abcdefghijklmnopqrstuvwxyz_ABCDEFGHIJKLMNOPQRSTUVWXYZ-0123456789-haha",
-//       // "交易所ViteX在4月16日正式上线公测版本。ViteX作为完全去中心化交易所具有链上撮合交易;、链上分红、交易信息链上完全透明、 私钥资产由用户自己保管、交易即挖矿、任何人都可以在ViteX交易所自主上币等特点。ViteX交易所公测地址： https://x-test.vite.net/为了方便您数字资产的保管，在体验交易所公测版本时，需要您重新注册账号，领取测试代币后即可体验交易（钱包的测试代币需要在交易所“资产管理”内充币至交易所）。在公测版本中我们将会开启以下14个交易对：GRIN.T/BTC、ETH/BTC、VITE/BTC、VTT.T/BTC、VTT.T/ETH、VITE/ETH、GRIN.T/ETH、GRIN.T/VITE、VTT.T/VITE、BTC/USDT、ETH/USDT、VITE/USDT、GRIN.T/USDT、VTT.T/USDT。最小交易额分别为：100VITE、0.01ETH，0.0005BTC，1USDT。公测版ViteX交易所仅有Web版本，下面为公测版本截图。",
-//       "全球第一个基于DAG的去中心化交易所ViteX在4月16日正式上线公测版本。ViteX作为完全去中心化交易所具有链上撮合交易;、链上分红、交易信息链上完全透明、 私钥资产由用户自己保管、交易即挖矿、任何人都可以在ViteX交易所自主上币等特点。ViteX交易所公测地址： https://x-test.vite.net/为了方便您数字资产的保管，在体验交易所公测版本时，需要您重新注册账号，领取测试代币后即可体验交易（钱包的测试代币需要在交易所“资产管理”内充币至交易所）。在公测版本中我们将会开启以下14个交易对：GRIN.T/BTC、ETH/BTC、VITE/BTC、VTT.T/BTC、VTT.T/ETH、VITE/ETH、GRIN.T/ETH、GRIN.T/VITE、VTT.T/VITE、BTC/USDT、ETH/USDT、VITE/USDT、GRIN.T/USDT、VTT.T/USDT。最小交易额分别为：100VITE、0.01ETH，0.0005BTC，1USDT。公测版ViteX交易所仅有Web版本，下面为公测版本截图。"
-//     ];
+    const notes = [
+      // null,
+      // "",
+      // "abcdefghijklmnopqrstuvwxyz",
+      // "abcdefghijklmnopqrstuvwxyz_ABCDEFGHIJKLMNOPQRSTUVWXYZ-0123456789",
+      // "abcdefghijklmnopqrstuvwxyz_ABCDEFGHIJKLMNOPQRSTUVWXYZ-0123456789-haha",
+      // "交易所ViteX在4月16日正式上线公测版本。ViteX作为完全去中心化交易所具有链上撮合交易;、链上分红、交易信息链上完全透明、 私钥资产由用户自己保管、交易即挖矿、任何人都可以在ViteX交易所自主上币等特点。ViteX交易所公测地址： https://x-test.vite.net/为了方便您数字资产的保管，在体验交易所公测版本时，需要您重新注册账号，领取测试代币后即可体验交易（钱包的测试代币需要在交易所“资产管理”内充币至交易所）。在公测版本中我们将会开启以下14个交易对：GRIN.T/BTC、ETH/BTC、VITE/BTC、VTT.T/BTC、VTT.T/ETH、VITE/ETH、GRIN.T/ETH、GRIN.T/VITE、VTT.T/VITE、BTC/USDT、ETH/USDT、VITE/USDT、GRIN.T/USDT、VTT.T/USDT。最小交易额分别为：100VITE、0.01ETH，0.0005BTC，1USDT。公测版ViteX交易所仅有Web版本，下面为公测版本截图。",
+      "全球第一个基于DAG的去中心化交易所ViteX在4月16日正式上线公测版本。ViteX作为完全去中心化交易所具有链上撮合交易;、链上分红、交易信息链上完全透明、 私钥资产由用户自己保管、交易即挖矿、任何人都可以在ViteX交易所自主上币等特点。ViteX交易所公测地址： https://x-test.vite.net/为了方便您数字资产的保管，在体验交易所公测版本时，需要您重新注册账号，领取测试代币后即可体验交易（钱包的测试代币需要在交易所“资产管理”内充币至交易所）。在公测版本中我们将会开启以下14个交易对：GRIN.T/BTC、ETH/BTC、VITE/BTC、VTT.T/BTC、VTT.T/ETH、VITE/ETH、GRIN.T/ETH、GRIN.T/VITE、VTT.T/VITE、BTC/USDT、ETH/USDT、VITE/USDT、GRIN.T/USDT、VTT.T/USDT。最小交易额分别为：100VITE、0.01ETH，0.0005BTC，1USDT。公测版ViteX交易所仅有Web版本，下面为公测版本截图。"
+    ];
 
-//     for (let index = 0; index < notes.length; index++) {
-//       let amount = `${index}000000000000000000`;
-//       const note = notes[index];
+    for (let index = 0; index < notes.length; index++) {
+      let amount = `${index}000000000000000000`;
+      const note = notes[index];
 
-//       let dataLength = null;
-//       if (note) {
-//         dataLength = Buffer.from(note, 'utf8').length
-//       }
-//       console.log('amount:', index, 'dataLength:', dataLength, 'note:', note);
+      let dataLength = null;
+      if (note) {
+        dataLength = Buffer.from(note, 'utf8').length
+      }
+      console.log('amount:', index, 'dataLength:', dataLength, 'note:', note);
 
-//       await sendBlock(
-//         accountAddress, 
-//         publicKey, 
-//         toAddress,
-//         amount, 
-//         tokenId, 
-//         fee, 
-//         note);
-//     }
+      await sendBlock(
+        accountAddress, 
+        publicKey, 
+        toAddress,
+        amount, 
+        tokenId, 
+        fee, 
+        note);
+    }
 
-//   } catch(err) {
-//     console.warn(err);
-//   }
-// }, 50000000); 
+  } catch(err) {
+    console.warn(err);
+  }
+}, 50000000); 
 
 test("vite.signLatestReceiveAccountBlock", async () => {
 
